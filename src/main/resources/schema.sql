1925c51b6f1f818966557141702908b034ac6245
jisbruzzi <jose.sbru@gmail.com>
2024-09-24 16:20:26 -0300

    delete everything

--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS transactions
-(
-    id uuid default random_uuid() primary key,
-    slogan varchar(255),
-    reference varchar(255),
-    created_at timestamp with time zone,
-    amount int
-);
